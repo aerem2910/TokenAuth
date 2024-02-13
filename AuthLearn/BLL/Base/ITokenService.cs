dcd7046e6b25d2806ad74f9c913595a55c6a6d32
Andrew <aerem2910@gmail.com>
2024-02-13 21:16:57 +0300

    token

---
+++ AuthLearn/BLL/Base/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthLearn.BLL.Base;
+
+{
+    public interface ITokenService
+    {
+        string GenerateToken(string email, string roleName, Guid userId, string additionalData);
+    }
+}
---
+++ AuthLearn/BLL/TokenService.cs
@@ -0,0 +1,41 @@
+﻿using AuthLearn.BLL.Base;
+using AuthLearn.Securities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AuthLearn.BLL
+{
+    public class TokenService : ITokenService
+    {
+        private readonly JwtConfiguration _jwt;
+
+        public TokenService(JwtConfiguration jwt)
+        {
+            _jwt = jwt;
+        }
+
+        public string GenerateToken(string email, string roleName, Guid userId, string additionalData)
+        {
+            var credentials = new SigningCredentials(_jwt.GetSigningKey(), SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, email),
+                new Claim(ClaimTypes.Role, roleName),
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()), // Добавлено ID пользователя
+                new Claim("additionalData", additionalData) // Добавлено дополнительное поле
+                // Добавьте другие метаданные пользователя, если необходимо
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _jwt.Issuer,
+                audience: _jwt.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
